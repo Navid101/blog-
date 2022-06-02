83809f95b680111ea67103a664197893341bb23c
Navid <navid.anjum@northsouth.edu>
2022-06-03 00:57:06 +0600

    author done

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,8 +1,21 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Author = () => {
+const Author = ({author}) => {
   return (
-    <div>Author</div>
+    <div className='text-center mt-20 mb-8 p-12 relative rounded-lg bg-white bg-opacity-20'>
+      <div className='absolute left-0 right-2 -top-14'>
+        <Image
+        unoptimized 
+        src={author.photo.url}
+        className='align-middle rounded-full object-cover'
+        height='100px'
+        width='100px'
+        alt={author.name} />
+      </div>
+      <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
+      <p className='text-white text-lg'>{author.bio}</p>
+    </div>
   )
 }
 
